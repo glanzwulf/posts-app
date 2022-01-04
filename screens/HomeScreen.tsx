8ac525b0047c878c303f155682d0f67327dd7f33
Vladyslav Chernyshov <ethel@live.com.pt>
2022-01-04 21:12:14 +0100

    feat: create cards and populate with api data

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,14 +1,52 @@
-import React from 'react'
-import { useTheme } from '@react-navigation/native'
-import { Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Text, View, SafeAreaView, FlatList, StyleSheet } from 'react-native'
+import axios from 'axios'
 
 export default function HomeScreen () {
-  const { colors } = useTheme()
+  const [postsData, setPostsData] = useState([])
 
-  return (
-    <View>
-      <Text style={{ color: colors.text }}>This will be the home screen</Text>
-      <Text style={{ color: colors.text }}>Here all the posts will appear</Text>
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => setPostsData(response.data));
+  }, []);
+
+  const Item = ({ title, id } : { title: string, id: number }) => (
+    <View style={styles.item}>
+      <Text style={styles.user_id}>user_id: {id}</Text>
+      <Text style={styles.title}>title: {title}</Text>
     </View>
   )
+
+  const renderItem = ({ item } : { item: any}) => (
+    <Item title={item.title} id={item.id} />
+  )
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={postsData}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 14,
+  },
+  user_id: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+});
