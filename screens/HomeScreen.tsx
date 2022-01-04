17f4c8299436a470e5361556bfea205f30af4334
Vladyslav Chernyshov <ethel@live.com.pt>
2022-01-04 17:24:38 +0100

    feat: set up basic screens and navigation

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function HomeScreen () {
+  return (
+    <View>
+      <Text>This will be the home screen</Text>
+      <Text>Here all the posts will appear</Text>
+    </View>
+  )
+}
