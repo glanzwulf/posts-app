61853c8135ea50b8837471defc58e715c4a3deb9
Vladyslav Chernyshov <ethel@live.com.pt>
2022-01-04 20:09:49 +0100

    feat: add and setup basic themes

--- App.tsx
+++ App.tsx
@@ -2,17 +2,36 @@
 import 'react-native-gesture-handler'
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
+import { AppearanceProvider, useColorScheme } from 'react-native-appearance'
+import { StatusBar } from 'expo-status-bar';
 import HomeScreen from './screens/HomeScreen'
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const scheme = useColorScheme()
+
+  const CustomDark = {
+    dark: true,
+    colors: {
+      primary: 'rgb(72, 45, 109)',
+      background: 'rgb(6, 9, 38)',
+      card: 'rgb(18, 20, 68)',
+      text: 'rgb(232, 232, 232)',
+      border: 'rgb(199, 199, 204)',
+      notification: 'rgb(72, 45, 109)',
+    },
+  };
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AppearanceProvider>
+      <NavigationContainer theme={scheme === 'dark' ? CustomDark : DefaultTheme}>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      <StatusBar style={scheme === 'dark' ? 'light' : 'dark'} />
+    </AppearanceProvider>
   );
 }
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useTheme } from '@react-navigation/native'
 import { Text, View } from 'react-native'
 
 export default function HomeScreen () {
+  const { colors } = useTheme()
+
   return (
     <View>
-      <Text>This will be the home screen</Text>
-      <Text>Here all the posts will appear</Text>
+      <Text style={{ color: colors.text }}>This will be the home screen</Text>
+      <Text style={{ color: colors.text }}>Here all the posts will appear</Text>
     </View>
   )
 }
