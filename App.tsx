541d3c2ccb2143e6c68549d7e4f840c539ee0980
Vladyslav Chernyshov <ethel@live.com.pt>
2022-01-08 11:02:23 +0100

    feat: add "add basic post button"

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 import { AppearanceProvider, useColorScheme } from 'react-native-appearance'
 import { StatusBar } from 'expo-status-bar';
 import HomeScreen from './screens/HomeScreen'
+import { Button } from 'react-native'
 
 const Stack = createStackNavigator();
 
@@ -28,7 +29,20 @@ export default function App() {
     <AppearanceProvider>
       <NavigationContainer theme={scheme === 'dark' ? CustomDark : DefaultTheme}>
         <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen 
+            name="Home"
+            component={HomeScreen} 
+            options={{
+              headerTitle: 'Posts App',
+              headerRight: () => (
+                <Button
+                  onPress={() => alert('This will open add menu')}
+                  title="Add Post"
+                  color="#00cc00"
+                />
+              ),
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
       <StatusBar style={scheme === 'dark' ? 'light' : 'dark'} />
